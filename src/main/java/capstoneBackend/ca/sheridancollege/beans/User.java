634f3ac0a11af22bc55970b621c6b0af16075a54
amninderkaur <amninder.ak11@gmail.com>
2026-05-01 23:18:22 -0400

    error fix

--- src/main/java/capstoneBackend/ca/sheridancollege/beans/User.java
+++ src/main/java/capstoneBackend/ca/sheridancollege/beans/User.java
@@ -30,7 +30,8 @@ public class User implements UserDetails{
     
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+        Role effectiveRole = role != null ? role : Role.USER;
+        return List.of(new SimpleGrantedAuthority(effectiveRole.name()));
     }
 
     @Override
