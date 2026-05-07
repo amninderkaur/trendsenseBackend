6911ff83050b7c323af452a7dc64904449749c53
amninderkaur <amninder.ak11@gmail.com>
2026-05-07 11:19:40 -0400

    corrretion

--- src/main/java/capstoneBackend/ca/sheridancollege/service/GeminiService.java
+++ src/main/java/capstoneBackend/ca/sheridancollege/service/GeminiService.java
@@ -103,7 +103,7 @@ public class GeminiService {
         try {
             Map<?, ?> response = geminiClient.post()
                     .uri(uriBuilder -> uriBuilder
-                            .path("/models/gemini-2.5-flash-preview-04-17:generateContent")
+                            .path("/models/gemini-2.5-flash-preview-image-generation:generateContent")
                             .queryParam("key", apiKey)
                             .build())
                     .header("Content-Type", "application/json")
